e4b311df8196a0a70733074d381893d91b7f99c7
edriso <edriso.dev@gmail.com>
2023-02-19 14:44:02 +0200

    create JWT_SECRET, JWT_LIFETIME variables in .env

--- models/User.js
+++ models/User.js
@@ -38,9 +38,13 @@ UserSchema.methods.getName = function () {
 };
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, 'jwtsecret', {
-    expiresIn: '30d',
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
 };
 
 module.exports = mongoose.model('User', UserSchema);
