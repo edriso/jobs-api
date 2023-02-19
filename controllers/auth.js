bf9f15aa48bcf0a13da79dabb6ba2e0c4108d69a
edriso <edriso.dev@gmail.com>
2023-02-19 12:37:41 +0200

    create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
-const register = (req, res) => {
-  res.send('register user');
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json(user);
 };
 
-const login = (req, res) => {
-  res.send('login user');
+const login = async (req, res) => {
+  res.status(StatusCodes.OK).json(req.body);
 };
 
 module.exports = {
