c5e29880f8da5d6b2fde2b5e0f2390074ef5bcba
edriso <edriso.dev@gmail.com>
2023-02-19 14:32:12 +0200

    generate token as a method of user instance

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
@@ -25,11 +26,21 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.methods.getName = function () {
+  return this.name;
+};
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtsecret', {
+    expiresIn: '30d',
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
