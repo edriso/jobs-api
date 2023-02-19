ae5c6eb0dfe242b08308d32863da9172a3c29273
edriso <edriso.dev@gmail.com>
2023-02-19 13:17:56 +0200

    update auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors/index');
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError('Please provide name, email, and password');
+  // }
 
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json(user);
 };
 
