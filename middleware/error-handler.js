b694220ff0691f70444c731e7b60cb348aae9b6e
edriso <edriso.dev@gmail.com>
2023-02-20 23:43:53 +0200

    handle validation errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -12,6 +12,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   //   return res.status(err.statusCode).json({ msg: err.message });
   // }
 
+  if (err.name === 'ValidationError') {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors)
+      .map((elm) => {
+        if (elm.properties.type === 'maxlength') {
+          return `${elm.properties.path}'s ${elm.properties.type} is ${elm.properties.maxlength} characters`;
+        }
+        return elm.message.toLowerCase();
+      })
+      .join(', ');
+  }
+
   if (err.code && err.code === 11000) {
     customError.statusCode = StatusCodes.BAD_REQUEST;
     customError.msg = `Duplicate value entered for ${Object.keys(
