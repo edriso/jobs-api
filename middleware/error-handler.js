99d095b946cf276b7922d6158b61ea1c593e60e5
edriso <edriso.dev@gmail.com>
2023-02-20 23:10:17 +0200

    remove if (err instanceof CustomAPIError)

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,15 +1,16 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+// const { CustomAPIError } = require('../errors');
 
+const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, try again later',
   };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
 
   if (err.code && err.code === 11000) {
     customError.statusCode = StatusCodes.BAD_REQUEST;
